37fb068f196a8b733f752907f73f38c3e8d7117b
root <root@varunsohal>
2024-04-16 20:59:24 -0500

    changing the admin_user.rb

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,12 +1,12 @@
-class AdminUser < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable,
-         :recoverable, :rememberable, :validatable
+# class AdminUser < ApplicationRecord
+#   # Include default devise modules. Others available are:
+#   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+#   devise :database_authenticatable,
+#          :recoverable, :rememberable, :validatable
 
-         def self.ransackable_attributes(auth_object = nil)
-          ["created_at", "email", "encrypted_password", "id", "id_value", "remember_created_at", "reset_password_sent_at", "reset_password_token", "updated_at"]
-        end
+#          def self.ransackable_attributes(auth_object = nil)
+#           ["created_at", "email", "encrypted_password", "id", "id_value", "remember_created_at", "reset_password_sent_at", "reset_password_token", "updated_at"]
+#         end
 
-        validates :email, presence: true, uniqueness: true
-end
+#         validates :email, presence: true, uniqueness: true
+# end
