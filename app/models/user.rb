0ec95632df79fd1212fe74979cb3f47988d6193b
root <root@varunsohal>
2024-04-16 21:04:00 -0500

    validating user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 has_one :cart
-         belongs_to :province
+        #  belongs_to :province
          validates :email, presence: true, uniqueness: true
          validates :province_id, presence: true
 end
