61a8974973253a75f80d01b8e65937b3e17ce82d
root <root@varunsohal>
2024-04-16 21:02:49 -0500

    validating product

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,6 +3,6 @@ class Contact < ApplicationRecord
     ["content", "created_at", "id", "id_value", "title", "updated_at"]
   end
 
-  # validates :title, presence: true
+  validates :title, presence: true
   validates :content, presence: true
 end
